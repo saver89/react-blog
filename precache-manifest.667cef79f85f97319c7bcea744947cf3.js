9e5c8fc8e8f03c362b03621ff9a7d929c8446d65
Vladimir <neelkim1989@gmail.com>
2019-11-29 23:13:14 +0500

    Updates

---
+++ precache-manifest.667cef79f85f97319c7bcea744947cf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d57313d0003cdae3483a2c3f8e409a",
+    "url": "/react-blog/index.html"
+  },
+  {
+    "revision": "f3e4850b7fd826e532a1",
+    "url": "/react-blog/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "af8287f5dce6f7c91574",
+    "url": "/react-blog/static/js/2.5412fa84.chunk.js"
+  },
+  {
+    "revision": "f3e4850b7fd826e532a1",
+    "url": "/react-blog/static/js/main.443a1cc4.chunk.js"
+  },
+  {
+    "revision": "c17628609b6b783b3b10",
+    "url": "/react-blog/static/js/runtime-main.0a20e50e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-blog/static/media/logo.25bf045c.svg"
+  }
+]);

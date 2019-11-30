38cf5e865eb153c76ad61abd215bd269b2608013
Vladimir <neelkim1989@gmail.com>
2019-11-30 23:38:04 +0500

    add blog theme as index page

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import Blog from './Blog';
 
-function greeter(person: string) {
-  return "Hello, " + person;
-}
-
-
-const App: React.FC = () => {
+function Copyright() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Container maxWidth="lg">
+      <Box my={4}>
+        <Blog />
+      </Box>
+    </Container>
+  );
+}

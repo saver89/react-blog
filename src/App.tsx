d930887331fce0acbcf1ea39ee7fc512793ad187
Vladimir <neelkim1989@gmail.com>
2019-12-01 23:31:20 +0500

    add post edit handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
@@ -21,20 +21,27 @@ function Copyright() {
   );
 }
 
-
-
-
 export default function App() {
   const [posts, setPost] = useState([
-    {id: 1, title: "POST1"},
-    {id: 2, title: "POST2"},
-    {id: 3, title: "POST3"}
+    { id: 1, title: "POST1" },
+    { id: 2, title: "POST2" },
+    { id: 3, title: "POST3" }
   ]);
 
   let detelePostHandler = (postIndex: number) => {
-    const dposts = [...posts];
-    dposts.splice(postIndex, 1);
-    setPost(dposts);
+    const tempPost = [...posts];
+    tempPost.splice(postIndex, 1);
+    setPost(tempPost);
+  };
+
+  let postEditedHandler = (event: any, id: any) => {
+    const postIndex = posts.findIndex(post => post.id == id);
+    const post = {... posts[postIndex]};
+    post.title = event.target.value;
+    const tempPosts = [...posts];
+    tempPosts[postIndex] = post;
+
+    setPost(tempPosts);
   };
 
   return (
@@ -43,7 +50,11 @@ export default function App() {
         <Typography variant="h4" component="h1" gutterBottom>
           Create React App v4-beta example with TypeScript
         </Typography>
-        {posts.map((post, index) => (<Person click={() => detelePostHandler(index)} key={post.id} id={post.id} title={post.title}/>))}
+        {posts.map((post, index) => {
+          return <Person changed={(event: any) => postEditedHandler(event, post.id)}
+            click={() => detelePostHandler(index)}
+            key={post.id} id={post.id} title={post.title} />;
+        })}
         <Copyright />
       </Box>
     </Container>
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -3,9 +3,13 @@ import React from 'react';
 interface post {
     title: string,
     id: number
-    click: any
+    click: any,
+    changed: any
 }
 
 export default function Post(props: post) {
-    return <div onClick={() => {props.click()}}>{props.title}</div>;
+    return <div>
+        <input type="text" onChange={(event) => props.changed(event)} />
+        <div onClick={() => { props.click() }}>{props.title}</div>
+        </div>;
 }

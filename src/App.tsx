1bcd8eb72c9315a7c3e0ad3721e0d76fc2dcd578
Vladimir <neelkim1989@gmail.com>
2019-12-01 22:32:33 +0500

    return to default empty pages with self post code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
-import Blog from './Blog';
+import post1 from './blog-post.1.md';
+import post2 from './blog-post.2.md';
+import post3 from './blog-post.3.md';
 
 function Copyright() {
   return (
@@ -18,11 +20,19 @@ function Copyright() {
   );
 }
 
+
+
 export default function App() {
+  const [posts, setPost] = useState([]);
+
   return (
-    <Container maxWidth="lg">
+    <Container maxWidth="sm">
       <Box my={4}>
-        <Blog />
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App v4-beta example with TypeScript
+        </Typography>
+        {posts.map(post => (<div>{post}</div>))}
+        <Copyright />
       </Box>
     </Container>
   );
